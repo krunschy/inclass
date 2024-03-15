39fa2e455cc90b0f1cba857fed3a133d7feb492c
krunschy <121090841+krunschy@users.noreply.github.com>
2024-03-15 10:29:17 +0100

    x gone

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,4 @@
-exxxport function Main({ children, type = 'normal' }) {
+export function Main({ children, type = 'normal' }) {
   const className = {
     normal: "min-h-[calc(100vh-329px)] py-8 px-2 sm:px-4 container mx-auto",
     reverse: "flex flex-col md:flex-row min-h-[calc(100vh-329px)] py-8 px-2 sm:px-4 container mx-auto",
