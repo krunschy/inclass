d2c623e4c1cc89acbcb03fd0edb5b05be33a7d4e
krunschy <121090841+krunschy@users.noreply.github.com>
2024-03-15 10:19:50 +0100

    ein x in main.tsx extra

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,4 @@
-export function Main({ children, type = 'normal' }) {
+exxport function Main({ children, type = 'normal' }) {
   const className = {
     normal: "min-h-[calc(100vh-329px)] py-8 px-2 sm:px-4 container mx-auto",
     reverse: "flex flex-col md:flex-row min-h-[calc(100vh-329px)] py-8 px-2 sm:px-4 container mx-auto",
